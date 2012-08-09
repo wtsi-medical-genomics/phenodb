28f1c78be51188e9ec969a8f4ca22fdad1795100
James Morris <jm20@sanger.ac.uk>
2012-08-09 09:44:46 +0000

    added showPhenotypes view

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from search.models import IndividualIdentifier
+from django.shortcuts import render, render_to_response
+from search.models import IndividualIdentifier, Phenotype
+from django.core.urlresolvers import reverse
 
 from django import forms
 
@@ -11,17 +12,34 @@ class SearchForm(forms.Form):
 def home(request):
     return render(request, 'search/home.html', {})
 
+def showPhenotypes(request):
+    query_results = Phenotype.objects.all()
+    return render_to_response('search/phenotypes.html', {'query_results': query_results})
+
 def idSearch(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid(): # All validation rules pass
-            ## get the string and forward to results view
-            
-            ## find the individual id 
-            print IndividualIdentifier.objects.filter(individual_string=form.cleaned_data['id_field'].strip()).count()
-            
-            
-            return  HttpResponseRedirect()
+            query_id = form.cleaned_data['id_field'].strip()
+            indIds = IndividualIdentifier.objects.filter(individual_string=query_id)
+            if IndividualIdentifier.objects.filter(individual_string=query_id).count() > 0:  
+                               
+                ## if there is more than one match the  print each one for the user to select the correct one                
+                if IndividualIdentifier.objects.filter(individual_string=form.cleaned_data['id_field'].strip()).count() > 1:
+                    print "ID matches multiple individuals:"
+                    for indId in indIds:
+                        print indId.source.source_name
+                else:
+                    indId = IndividualIdentifier.objects.get(individual_string=query_id)
+                
+                
+                url = reverse('idresults', kwargs={'indId': indId.individual.id})
+
+                HttpResponseRedirect(url)
+                
+            else:
+                ## id not found
+                HttpResponseRedirect()
     else:
         form = SearchForm() # An unbound form
         return render(request, 'search/idsearch.html', {'form': form,})
