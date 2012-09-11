9672018d279b58bee276ff79a1a3214e748d5bb8
James Morris <jm20@sanger.ac.uk>
2012-09-11 14:26:00 +0000

    using textarea now

--- search/views.py
+++ search/views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 from django import forms
 
 class SearchForm(forms.Form):
-    id_field = forms.CharField(max_length=100)
+    id_textarea = forms.widgets.Textarea(attrs={'rows':4, 'cols':60})
 
 def home(request):
     return render(request, 'search/home.html', {})
@@ -20,18 +20,25 @@ def idSearch(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid(): # All validation rules pass
-            query_id = form.cleaned_data['id_field'].strip()    
-            query_results = IndividualIdentifier.objects.filter(individual_string=query_id)
-            if query_results.count() > 0:                
-                inds = []                
-                for ind in query_results:
-                    sample_results = Sample.objects.filter(individual=ind)                    
-                    affection_results = AffectionStatusPhenotypeValue.objects.filter(individual=ind)                     
-                    qualitative_results = QualitativePhenotypeValue.objects.filter(individual=ind)                    
-                    quantitiative_results = QuantitiatvePhenotypeValue.objects.filter(individual=ind)
-                    ind_results = {'ind':ind,'samples':sample_results,'aff':affection_results,'qual':qualitative_results,'quant':quantitiative_results}
-                    inds.append(ind_results)
-                return render_to_response('search/idresults.html', {'query_results': inds})
+            
+            ##Êcheck which form field has been used
+            textarea_values = form.cleaned_data['id_textarea'].splitlines()
+            
+            ## for each id
+            for line in textarea_values:
+                query_id = line.strip()
+                query_results = IndividualIdentifier.objects.filter(individual_string=query_id)
+                
+                if query_results.count() > 0:                
+                    inds = []                
+                    for ind in query_results:
+                        sample_results = Sample.objects.filter(individual=ind)                    
+                        affection_results = AffectionStatusPhenotypeValue.objects.filter(individual=ind)                     
+                        qualitative_results = QualitativePhenotypeValue.objects.filter(individual=ind)                    
+                        quantitiative_results = QuantitiatvePhenotypeValue.objects.filter(individual=ind)
+                        ind_results = {'ind':ind,'samples':sample_results,'aff':affection_results,'qual':qualitative_results,'quant':quantitiative_results}
+                        inds.append(ind_results)
+                    return render_to_response('search/idresults.html', {'query_results': inds})
 #            else:
 #            id not found
 
