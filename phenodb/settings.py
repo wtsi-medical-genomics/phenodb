c1cff18f5fa3944982e4ae0bdfd70e4f78c50666
James Morris <jm20@sanger.ac.uk>
2012-09-25 12:17:54 +0000

    added django-tables2 app

--- phenodb/settings.py
+++ phenodb/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'search'
+    'django_tables2'
 )
 
 # A sample logging configuration. The only tangible logging
