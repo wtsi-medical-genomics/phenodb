f5608448cfb38b9d7a46d4460c73457dacdd428e
James Morris <jm20@sanger.ac.uk>
2013-01-28 10:53:35 +0000

    added to version control

---
+++ search/tables.py
@@ -0,0 +1,51 @@
+import django_tables2 as tables
+from search.models import Phenotype, Platform, Study, QC, Source, Individual, Sample
+
+class PhenotypeTable(tables.Table):        
+    class Meta:
+        model = Phenotype
+        fields = ('phenotype_name', 'phenotype_description', 'phenotype_type')
+        attrs = {'class': 'table table-striped table-bordered'}
+
+class PlatformTable(tables.Table):        
+    class Meta:
+        model = Platform
+        fields = ('platform_name', 'platform_type', 'platform_description')
+        attrs = {'class': 'table table-striped table-bordered'}
+
+class StudyTable(tables.Table):        
+    class Meta:
+        model = Study
+        fields = ('study_name', 'study_name', 'data_location', 'study_description')         
+        attrs = {'class': 'table table-striped table-bordered'}
+        
+class QCTable(tables.Table):        
+    class Meta:
+        model = QC
+        fields = ('qc_name', 'qc_description')         
+        attrs = {'class': 'table table-striped table-bordered'}
+        
+class SourceTable(tables.Table):        
+    class Meta:
+        model = Source
+        fields = ('source_name', 'contact_name', 'source_description')         
+        attrs = {'class': 'table table-striped table-bordered'}
+
+class IndividualTable(tables.Table):        
+    class Meta:
+        model = Individual
+        fields = ('id', 'sex')         
+        attrs = {'class': 'table table-striped table-bordered'}
+        
+class SampleTable(tables.Table):        
+    class Meta:
+        model = Sample
+        fields = ('sample_id')         
+        attrs = {'class': 'table table-striped table-bordered'}
+        
+class CountTable(tables.Table):        
+    name = tables.Column()
+    count = tables.Column()
+    
+    class Meta:         
+        attrs = {'class': 'table table-striped table-bordered'}                
