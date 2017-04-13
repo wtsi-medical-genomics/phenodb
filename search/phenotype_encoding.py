ccc105f70342821c1d83e8889177e9c9bf3ddb17
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-04-13 14:46:41 +0100

    Create collection of dictionaries of phenotype encodings/conversions.

---
+++ search/phenotype_encoding.py
@@ -0,0 +1,23 @@
+sex = {
+    '0'     : 'Unknown',
+    
+    '1'     : 'Male',
+    'm'     : 'Male',
+    'male'  : 'Male',
+    
+    '2'     : 'Female',
+    'f'     : 'Female',
+    'female': 'Female',
+}
+
+binary = {
+    'yes'       : True,
+    '1'         : True,
+    'affected'  : True,
+    'true'      : True,
+
+    'no'        : False,
+    'false'     : False,
+    '0'         : False,
+    'unaffected': False,
+}
