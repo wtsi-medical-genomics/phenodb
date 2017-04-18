2b496ac7c48b6c083b18a2c101d0f2660fb31b3a
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-04-18 16:52:42 +0100

    Use sqlite, reduce middleware, and turn off debug to speed up testing.

--- manage.py
+++ manage.py
@@ -4,6 +4,29 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phenodb.settings")
+
+    if 'test' in sys.argv:
+        from django.conf import settings
+        import logging
+        logging.disable(logging.CRITICAL)
+        settings.DEBUG = False
+        settings.TEMPLATE_DEBUG = False
+        settings.PASSWORD_HASHERS = [
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+        ]
+        settings.DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test_database',
+            }
+        }
+        settings.MIDDLEWARE = [
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ]
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
